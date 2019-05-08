806f7771b3d7ac731ff1b3c75df1b0e1e53c566d
96no3 <g6no3d@gmail.com>
2019-05-08 15:41:01 +0900

    テキスト第二回　課題01　スタッククラス定義
    スタッククラス内にpushとpop関数を定義

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -3,7 +3,38 @@
 */
 #include <iostream>
 
+class Stack {
+public:
+
+	void push(int num) {
+		sArray[count] = num;
+		count++;
+	}
+
+	int pop() {	
+		count > 0 ? count-- : 0;		
+		return sArray[count];
+	}	
+
+private:
+	unsigned int sArray[10];
+	unsigned count = 0;
+};
+
+
+
 int main() {
-	std::cout << "Hello World" << std::endl;
+	/*std::cout << "Hello World" << std::endl;*/
+
+	Stack s;
+
+	s.push(10);	
+	s.push(30);
+	std::cout << s.pop() << std::endl;
+	s.push(20);
+	s.push(100);
+	s.pop();
+	std::cout << s.pop() << std::endl;
+	std::cout << s.pop() << std::endl;
 	return 0;
 }
